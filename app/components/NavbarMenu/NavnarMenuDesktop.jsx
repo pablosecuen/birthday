d435a7c5a8c97c605d0ef3be28aa13d3dbe6fdf4
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-08-03 23:17:43 -0300

    padding nav bar menu

--- app/components/NavbarMenu/NavnarMenuDesktop.jsx
+++ app/components/NavbarMenu/NavnarMenuDesktop.jsx
@@ -6,7 +6,7 @@ import logo from "../../assets/logo/logo.png";
 
 function NavnarMenuDesktop() {
   return (
-    <div className="flex justify-center items-center w-full mx-auto px-64 align-middle">
+    <div className="flex justify-center items-center w-full mx-auto px-96 align-middle">
       {" "}
       <ul className="flex gap-10 justify-evenly transition duration-500 items-center w-full">
         <li className="my-2 hover:text-secondary transition duration-700 drop-shadow-xl shadow-black hover:scale-125 ">
