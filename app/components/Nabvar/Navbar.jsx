351b320947c81bf40efe26c5cd48ff11aed2a9f0
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-08-03 23:19:18 -0300

    fix logo

--- app/components/Nabvar/Navbar.jsx
+++ app/components/Nabvar/Navbar.jsx
@@ -5,6 +5,7 @@ import NavbarMenu from "../NavbarMenu/NavbarMenu";
 import NavnarMenuDesktop from "../NavbarMenu/NavnarMenuDesktop";
 import UseAnimations from "react-useanimations";
 import menu2 from "react-useanimations/lib/menu2";
+import logo from "../../assets/logo/logo.png";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -31,9 +32,17 @@ const Navbar = () => {
     <>
       <div className="z-50 fixed h-24 bg-transparent  -pl-8 border-white w-screen flex items-center justify-between align-middle lg:pr-8 bg-metal2 bg-center bg-contain backdrop-blur-2xl">
         {isMobile ? (
-          <button onClick={toggleMenu} className=" animation-container pr-4">
-            <UseAnimations animation={menu2} size={50} fillColor="#ffffff" strokeColor="#ffffff" />
-          </button>
+          <>
+            <Image src={logo} alt="logo" className="h-18 sm:w-20 w-16 ml-4 sm:ml-10" />
+            <button onClick={toggleMenu} className=" animation-container pr-4">
+              <UseAnimations
+                animation={menu2}
+                size={50}
+                fillColor="#ffffff"
+                strokeColor="#ffffff"
+              />
+            </button>
+          </>
         ) : (
           <>
             <NavnarMenuDesktop />
