155905a45a937065cca3cbba20aec62658315cf1
pablosecuen <pablosecuen@gmail.com>
2023-08-06 18:29:03 -0300

    fix color text

--- app/components/info/Info.jsx
+++ app/components/info/Info.jsx
@@ -22,7 +22,7 @@ function Info() {
   return (
     <section
       id="info"
-      className="flex relative flex-col mt-32 md:mt-72 w-[80vw] h-full justify-evenly items-center md:flex-row md:h-[500px] gap-20 "
+      className="flex relative text-white flex-col mt-32 md:mt-72 w-[80vw] h-full justify-evenly items-center md:flex-row md:h-[500px] gap-20 "
     >
       {!isMobile && (
         <Image
@@ -38,14 +38,16 @@ function Info() {
         <section
           key={section.id}
           id="evento"
-          className=" w-[350px] h-full flex flex-col items-center  rounded-3xl justify-center p-4 bg-card  bg-contain  bg-center  bg-no-repeat md:bg-none"
+          className=" w-[350px]  text-white h-full flex flex-col items-center  rounded-3xl justify-center p-4 bg-card  bg-contain  bg-center  bg-no-repeat md:bg-none"
         >
-          <div className="relative text-center flex flex-col items-center ">
-            <h2 className="card pb-4 font-bold text-center text-2xl contrast-100 font-azonix ">
+          <div className="relative  text-white text-center flex flex-col items-center ">
+            <h2 className="card  text-white pb-4 font-bold text-center text-2xl contrast-100 font-azonix ">
               {section?.title}
             </h2>
-            <h3 className="card  rounded-2xl pb-4 text-md">{section.content?.subtitle}</h3>
-            <span className="card text-sm"> {section.content.description}</span>
+            <h3 className="card  text-white rounded-2xl pb-4 text-md">
+              {section.content?.subtitle}
+            </h3>
+            <span className="card  text-white text-sm"> {section.content.description}</span>
           </div>
         </section>
       ))}
