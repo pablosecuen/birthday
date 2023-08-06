9b14bec5386c0ac17ed9116c2075fd1b57d7e964
pablosecuen <pablosecuen@gmail.com>
2023-08-06 15:08:02 -0300

    fix

--- app/components/preloader/preloader.jsx
+++ app/components/preloader/preloader.jsx
@@ -2,11 +2,14 @@
 import React from "react";
 import Image from "next/image";
 import Script from "next/script";
+import zodiac from "../../assets/spinner/zodiac.gif";
 
 const Preloader = () => {
   return (
     <div className="preloader-container flex justify-center items-center align-middle fixed top-0 left-0 w-screen h-screen  z-50">
-      <div className="preloader animate animate-pulse transition duration-1000">LOADING...</div>
+      <div className="preloader animate animate-pulse transition duration-1000">
+        <Image src={zodiac} alt="loading..." />
+      </div>
     </div>
   );
 };
