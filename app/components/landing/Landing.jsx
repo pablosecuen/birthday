6701cebbc89b14d7276f27bb4272613ad3b20598
pablosecuen <pablosecuen@gmail.com>
2023-08-06 15:22:54 -0300

    button fix

--- app/components/landing/Landing.jsx
+++ app/components/landing/Landing.jsx
@@ -95,13 +95,13 @@ function Landing() {
       </div>
       {isMobile && (
         <button
-          className="bg-transparent text-white  text-xs font-bold py-2 px-4 flex flex-col  z-40 absolute bottom-6 border rounded-2xl animate-pulse"
+          className="bg-transparent text-white  text-xs font-bold py-2 px-4 flex flex-col  z-50 absolute bottom-6 border rounded-2xl animate-pulse"
           onClick={handleSmoothScroll}
         >
           Revela el misterio tocando aquí
           <p className="text-center mx-auto text-xl">▽</p>
         </button>
-      )}
+      )}{" "}
     </>
   );
 }
